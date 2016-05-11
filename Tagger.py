f67ebe38b715cd900c82a4d0c42891e343b41666
Nicolas Lehmann <mail@nicolaslehmann.de>
2016-05-11 14:07:22 +0200

    update

--- Tagger.py
+++ Tagger.py
@@ -14,32 +14,28 @@ def main():
     print("\nPreparing needed lists...\n")
 
     # streets
-    f_roads_london = open('roads_roma.txt', 'rb')                                                                       # open file
-    for tmp in csv.reader(f_roads_london, delimiter=';'):                                                                              # save content to dictionary
-        # tmp = str(row).split(";")                                                                                       # |
-        if len(tmp) < 10:
-            print(tmp)
-        list_entry = [tmp[2].replace('"',''), tmp[9].replace('"', '').replace('\'', '').replace(']', '')]               # |
+    f_roads_london = open('london_roads.csv', 'rb')                                                                     # open file
+    for tmp in csv.reader(f_roads_london, delimiter=';'):                                                               # save content to dictionary
+        list_entry = [tmp[0], tmp[1]]                                                                                   # |
         list_london_roads.append(list_entry)                                                                            # |
     f_roads_london.close()                                                                                              # close file
     print("\nFilling list with roads = done...\n")
 
                                                                                                                         # ADMIN LEVELS
-    f_admin_london = open('admin_roma.txt', 'rb')                                                                       # open file
-    for row in csv.reader(f_admin_london):                                                                              # save content to list
-        tmp = str(row).split(";")                                                                                       # |
-        list_entry = [tmp[1],tmp[2],tmp[3]]                                                                             # |
+    f_admin_london = open('london_admin.csv', 'rb')                                                                     # open file
+    for tmp in csv.reader(f_admin_london, delimiter=';'):                                                               # save content to list
+        if len(tmp)>3:
+            print tmp
+        list_entry = [tmp[0], tmp[1], tmp[2]]                                                                           # |
         list_london_adminlevels.append(list_entry)                                                                      # |
     f_admin_london.close()                                                                                              # close file
     print("\nFilling lists with admin levels = done...\n")
 
                                                                                                                         # ARTICLES
 
-    f_articles_london = open('londonarticles.csv', 'rb')                                                                # open file
-    for tmp in csv.reader(f_articles_london, delimiter='|'):                                                                           # save content to dictionary
-        #tmp = str(row).split("|")                                                                                       # |
-        if len(tmp) < 3:
-            print(tmp)
+    f_articles_london = open('london_articles.csv', 'rb')                                                               # open file
+    for tmp in csv.reader(f_articles_london, delimiter='|'):                                                            # save content to dictionary
+        print(tmp)                                                                                                      # |
         list_entry = [tmp[0].replace('[', '').replace('\'', ''), tmp[1].replace('\'', ''), tmp[2]]                      # |
         list_london_articles.append(list_entry)                                                                         # |
     f_articles_london.close()                                                                                           # close file
@@ -52,8 +48,8 @@ def main():
         for road  in list_london_roads:                                                                                 # |
             for article in list_london_articles:                                                                        # |
                 if (road[0] in article[2]):                                                                             # |
-                    if len(road) < 3:
-                        print(road)
+                    if len(road) < 3:                                                                                   # |
+                        print(road)                                                                                     # |
                     article.append(road[1])                                                                             # | tag article with a position (road)
                     isGeotagged = True                                                                                  # | mark article as tagged
 
